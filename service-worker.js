82e34d77097a7f350bd6964c2ca7cc18d7a5c1b5
Shawn <salley8795@gmail.com>
2024-06-08 17:48:39 -0500

    Added service worker registration and caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,37 @@
+const cacheName = 'quick-calc-cache-v1';
+const resourcesToPrecache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/styles.css', // if you have a separate CSS file
+  '/script.js' // if you have a separate JS file
+];
+
 self.addEventListener('install', event => {
+  console.log('Service worker install event!');
   event.waitUntil(
-    caches.open('quick-calc-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/service-worker.js',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
-      ]);
-    })
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(resourcesToPrecache);
+      })
   );
 });
 
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
 self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
   );
 });
